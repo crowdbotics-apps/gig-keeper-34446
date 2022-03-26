685fd629d32f93fb1f78962a2e2a43bcde2f3740
Crowdbotics <team@crowdbotics.com>
2022-03-26 11:58:30 +0000

    Initial commit.

---
+++ backend/gig_keeper_34446/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gig_keeper_34446 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gig_keeper_34446.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "gig_keeper_34446",
+  url: "https://gig_keeper_34446.botics.co",
+  api: "https://gig_keeper_34446.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
